7e1a703e7518835054740ec1e5e9ce640be7d283
Gyan chand Mohanty <gyanchand.mohanty@gmail.com>
2019-04-23 10:34:32 +0530

    number system

---
+++ number.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 22:25:58 2019
+
+@author: Gyan
+"""
+
+x = 12
+y = 3.2
+
+print("12 + 3.2 = ",x+y)
+
+print("12 - 3.2 = ",x-y)
+
+print("12 X 3.2 = ",x*y)
+print("12 / 3.2 = ",x/y)
+
+print("12 // 3.2 = ",x//y)
+
+2+2*10-5
+
