875a1cd3b7fa742d296d02da24fe901134dfc74c
Gyan chand Mohanty <gyanchand.mohanty@gmail.com>
2019-04-23 10:32:59 +0530

    python basics

---
+++ basic.py
@@ -0,0 +1,27 @@
+print("hello world")
+print('hello')
+
+#single quote inside double quote
+print("how's your day?\nhi")
+
+#double quote inside single quote
+print('"python" is awesome...')
+
+#single quote as well as double quote
+print(""""python" isn't "cool"!!! """)
+
+# print('hi how's life?')  #syntax error
+
+2+3
+
+print(25)
+
+print(2*3)
+
+greet="hi"
+print(greet)
+
+_Name='gyan'
+print(_Name)
+
+print(greet,_Name)
